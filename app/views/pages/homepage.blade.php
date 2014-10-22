4b719174cd16adfad378f82010d1e3b5fa069bb8
marcel pirnay <pirnay.m@gmail.com>
2014-10-22 13:23:40 +0200

    homepage avancement

--- app/views/pages/homepage.blade.php
+++ app/views/pages/homepage.blade.php
@@ -2,5 +2,19 @@
 
 
 @section('content')
-    <p>Homepage</p>
+    <div class="home__introduction">
+      <section class="home__introduction__event">
+          <div class="home__introduction__event__wrapper">
+            <h2 class="home__introduction__event__wrapper__title">Prochain évenement <span>au centre de Liège</span></h2>
+            <date class="home__introduction__event__wrapper__date">Vendredi 22 octobre 2014</date>
+            <a href="" class="home__introduction__event__wrapper__link">Réserver</a>
+          </div>
+      </section>
+      <div class="home__introduction__logo">
+        <img src="./img/logo.svg" alt="" class="home__introduction__logo__image">
+      </div>
+    </div>
+    <div class="home__presentation">
+      
+    </div>
 @stop
